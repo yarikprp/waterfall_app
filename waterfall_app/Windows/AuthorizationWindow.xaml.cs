38129d8327cee1aaa35a678cfbae983b63f361a5
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2024-10-20 11:38:08 +0300

    Добавил авторизацию

---
+++ waterfall_app/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace waterfall_app.Windows
+{
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (username == "admin" && password == "123")
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль.");
+            }
+        }
+    }
+}
